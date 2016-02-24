d37d55fa0f391f8428d9ff2c70ef98d0b470e4a9
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-02-24 16:27:04 -0600

    Terminando Clase Estudiante, y subiendo tabla Estudiantes!

--- BLL/Estudiantes.cs
+++ BLL/Estudiantes.cs
@@ -50,25 +50,83 @@ namespace BLL
         }
         public override bool Insertar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            try
+            {
+                retorno = conexion.Ejecutar(string.Format("Insert Into Estudiantes(Matricula,Nombre,Apellido,Genero,FechaNacimiento,Celular,Email, Direccin,ToturId) values({0},'{1}','{2}','{3}',{4},'{5}','{6}','{7}',{})",this.Matricula,this.Nombre,this.Apellidos,this.Genero,this.FechaNacimiento,this.Celular,this.Email, this.Direccion, this.TutorId));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return retorno;
         }
         public override bool Editar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            try
+            {
+                retorno = conexion.Ejecutar(string.Format("update Estudiantes set Matricula= {0}, Nombre= '{1}', Apellido='{2}', Genero='{3}', FechaNacimiento='{4}', Celular='{5}',Email='{6}',Direccion='{7}',TutorId={8} where EstudianteId= {9}", this.Matricula, this.Nombre, this.Apellidos, this.Genero, this.FechaNacimiento, this.Celular, this.Email, this.Direccion, this.TutorId, this.EstudianteId));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return retorno;
         }
 
         public override bool Eliminar()
         {
-            throw new NotImplementedException();
+            bool retorno = false;
+            try
+            {
+                retorno = conexion.Ejecutar(string.Format("delete from Estudiantes where EstudianteId= {0}", this.EstudianteId));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return retorno;
         }
         public override bool Buscar(int IdBuscado)
         {
-            throw new NotImplementedException();
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = conexion.ObtenerDatos(string.Format("select * From Estudiantes where EstudianteId="+ IdBuscado));
+                if (dt.Rows.Count>0)
+                {
+                    this.EstudianteId = (int)dt.Rows[0]["EstudianteId"];
+                    this.Matricula = (int)dt.Rows[0]["Matricula"];
+                    this.Nombre = dt.Rows[0]["Nombre"].ToString();
+                    this.Apellidos = dt.Rows[0]["Apellido"].ToString();
+                    this.Genero = dt.Rows[0]["Genero"].ToString();
+                    this.FechaNacimiento = dt.Rows[0]["FechaNacimiento"].ToString();
+                    this.Celular = dt.Rows[0]["Celular"].ToString();
+                    this.Email = dt.Rows[0]["Email"].ToString();
+                    this.Direccion = dt.Rows[0]["Direccion"].ToString();
+                    this.TutorId = (int)dt.Rows[0]["TutorId"];
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return dt.Rows.Count > 0;
         }
 
         public override DataTable Listado(string Campos, string Condicion, string Orden)
         {
-            throw new NotImplementedException();
+            string ordenFinal = "1=1";
+            if (!Orden.Equals(""))
+                ordenFinal = " Orden by  " + Orden;
+
+            return conexion.ObtenerDatos("Select " + Campos + " From Estudiantes Where " + Condicion + Orden);
         }
     }
 }
--- DAL/scripts/TeacherControlDb.sql
+++ DAL/scripts/TeacherControlDb.sql
@@ -4,4 +4,19 @@
  clave varchar(15),
  tipoUsuario varchar(20),
  primary key(usuarioId)
-);
+) 
+Go
+Create table Estudiantes( 
+		EstudianteId int identity(1,1),
+		Matricula int,
+		Nombre varchar(50),
+		Apellido varchar(30),
+		Genero VarChar(5),
+		FechaNacimiento varchar(20),
+		Celular varchar(15),
+		Email Varchar(70),
+		Direccion varchar(100),
+		TutorId int,
+		primary key(EstudianteId)
+)
+Go
