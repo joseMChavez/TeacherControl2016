b040c19c707686cbfb84c98f680175d61ae5b9e2
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-03-26 16:43:31 -0400

    Agregando ReportView Generico

---
+++ TeacherControl2016/ReporteForm/ReportViewGenerico.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TeacherControl2016.ReporteForm
+{
+    partial class ReportViewGenerico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewerGenerico = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewerGenerico
+            // 
+            this.reportViewerGenerico.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewerGenerico.Location = new System.Drawing.Point(0, 0);
+            this.reportViewerGenerico.Name = "reportViewerGenerico";
+            this.reportViewerGenerico.Size = new System.Drawing.Size(710, 290);
+            this.reportViewerGenerico.TabIndex = 0;
+            // 
+            // ReportViewGenerico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 290);
+            this.Controls.Add(this.reportViewerGenerico);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ReportViewGenerico";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.ReportViewGenerico_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewerGenerico;
+    }
+}
---
+++ TeacherControl2016/ReporteForm/ReportViewGenerico.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeacherControl2016.ReporteForm
+{
+    public partial class ReportViewGenerico : Form
+    {
+        public string reporte { get; set; }
+        public DataTable data { get; set; }
+        public ReportViewGenerico()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportViewGenerico_Load(object sender, EventArgs e)
+        {
+            this.reportViewerGenerico.Reset();
+            this.reportViewerGenerico.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Local;
+
+            this.reportViewerGenerico.LocalReport.ReportPath = this.reporte;
+
+            ReportDataSource source = new ReportDataSource(this.data.TableName, this.data);
+
+            this.reportViewerGenerico.LocalReport.DataSources.Add(source);
+
+           
+            this.reportViewerGenerico.RefreshReport();
+        }
+    }
+}
